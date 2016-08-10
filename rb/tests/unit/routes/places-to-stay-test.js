a7bed5603e33e230e386c042def8fd03d91004dc
Marine Boudeau <m14868@gmail.com>
2016-08-09 20:53:57 -0400

    Added new files.

---
+++ rb/tests/unit/routes/places-to-stay-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:places-to-stay', 'Unit | Route | places to stay', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
