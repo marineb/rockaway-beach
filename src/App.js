7874b38ed75a97151a25b0cea1903cc550cca262
Marine Boudeau <m14868@gmail.com>
2017-05-14 12:48:56 -0400

    Introduce react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Rockaway Beach</h2>
+        </div>
+        <p className="App-intro">
+          We are currently under maintenance. We'll be back by the end of the day.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
