525cd9e4fef4371a6f66894181a261a32123fffc
Marine Boudeau <m14868@gmail.com>
2017-05-14 14:33:07 -0400

    Add react-snapshot to improve SEO

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  render(<App />, div);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import App from './App';
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
 );
